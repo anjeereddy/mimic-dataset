a88298b68e8d30d2b19a57149bb5c5d6ca0e3f4d
anjeereddy <ram@tcs.com>
2026-03-22 23:12:02 +0530

    initial version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mimic_project_pipeline",
-    version="0.1.0",
+    version="0.1.1",
     install_requires=[
         "databricks-sdk",
         "PyYAML==6.0.2",
