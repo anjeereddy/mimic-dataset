1c284e74805e560f28c335d241c5c396e51d67b0
anjeereddy <ram@tcs.com>
2026-03-28 22:43:43 +0530

    corrected the version

--- src/mimic_dataset/utils/globals.py
+++ src/mimic_dataset/utils/globals.py
@@ -1,11 +1,8 @@
 from pyspark.sql import SparkSession
 
-
 class GlobalVariables:
     spark: SparkSession = None
 
     @classmethod
     def setup_spark(cls):
         GlobalVariables.spark = SparkSession.builder.getOrCreate()
-
-        GlobalVariables.spark.conf.set("spark.databricks.delta.optimizeWrite.enabled", "true")
